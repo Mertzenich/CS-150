b45f1ce8d1e11662059e53590c36b74fe515405b
Adam Mertzenich <mertad01@luther.edu>
2020-10-17 22:17:55 -0500

    Update

--- project.py
+++ project.py
@@ -26,95 +26,123 @@ def promptSolve():
 
 
 app = tk.Tk()
-numFrame = Frame(app)
-numFrame.pack(side=LEFT)
+mainFrame = Frame(app)
+mainFrame.pack()
 
-numFrame1 = Frame(numFrame)
+left = Frame(mainFrame)
+left.pack(side=LEFT)
+
+right = Frame(mainFrame)
+right.pack(side=RIGHT)
+
+numFrame1 = Frame(left)
 numFrame1.pack(side=BOTTOM)
 
-numFrame2 = Frame(numFrame)
+numFrame2 = Frame(left)
 numFrame2.pack(side=BOTTOM)
 
-numFrame3 = Frame(numFrame)
+numFrame3 = Frame(left)
 numFrame3.pack(side=BOTTOM)
 
-commandFrame = Frame(app)
-commandFrame.pack(side=RIGHT)
+commandFrame = Frame(right)
+commandFrame.pack()
 
 promptBox = tk.Label(commandFrame, text="")
 
+###
+### Command Frame
+###
 
-buttonAnswer = tk.Button(commandFrame, text="Answer", width=30,
+buttonAnswer = tk.Button(commandFrame, text="=", width=1,
                           command=partial(promptSolve))
 
 
-buttonBackspace = tk.Button(commandFrame, text="Backspace", width=30,
+buttonBackspace = tk.Button(commandFrame, text="X", width=1,
                           command=partial(promptRemove, 1))
 
 
-buttonZero = tk.Button(numFrame, text="0", width=30,
+buttonZero = tk.Button(mainFrame, text="0",
                           command=partial(promptAppend, '0'))
 
+###
+### Number Frame 1
+###
 
-buttonOne = tk.Button(numFrame1, text="1", width=30,
+buttonOne = tk.Button(numFrame1, text="1",
                           command=partial(promptAppend, '1'))
 
 
-buttonTwo = tk.Button(numFrame1, text="2", width=30,
+buttonTwo = tk.Button(numFrame1, text="2",
                           command=partial(promptAppend, '2'))
 
 
-buttonThree = tk.Button(numFrame1, text="3", width=30,
+buttonThree = tk.Button(numFrame1, text="3",
                           command=partial(promptAppend, '3'))
 
+###
+### Number Frame 2
+###
 
-buttonFour = tk.Button(numFrame2, text="4", width=30,
+buttonFour = tk.Button(numFrame2, text="4",
                           command=partial(promptAppend, '4'))
 
 
-buttonFive = tk.Button(numFrame2, text="5", width=30,
+buttonFive = tk.Button(numFrame2, text="5",
                           command=partial(promptAppend, '5'))
 
 
-buttonSix = tk.Button(numFrame2, text="6", width=30,
+buttonSix = tk.Button(numFrame2, text="6",
                           command=partial(promptAppend, '6'))
 
 
-buttonSeven = tk.Button(numFrame3, text="7", width=30,
+###
+### Number Frame 3
+###
+
+
+buttonSeven = tk.Button(numFrame3, text="7",
                           command=partial(promptAppend, '7'))
 
 
-buttonEight = tk.Button(numFrame3, text="8", width=30,
+buttonEight = tk.Button(numFrame3, text="8",
                           command=partial(promptAppend, '8'))
 
 
-buttonNine = tk.Button(numFrame3, text="9", width=30,
+buttonNine = tk.Button(numFrame3, text="9",
                           command=partial(promptAppend, '9'))
 
+###
+### Command Frame
+###
 
-buttonPlus = tk.Button(commandFrame, text="+", width=30,
+buttonPlus = tk.Button(commandFrame, text="+", width=1,
                           command=partial(promptAppend, '+'))
 
 
-buttonMin = tk.Button(commandFrame, text="-", width=30,
+buttonMin = tk.Button(commandFrame, text="-", width=1,
                           command=partial(promptAppend, '-'))
 
 
-buttonMul = tk.Button(commandFrame, text="*", width=30,
+buttonMul = tk.Button(commandFrame, text="*", width=1,
                           command=partial(promptAppend, '*'))
 
 
-buttonDiv = tk.Button(commandFrame, text="/", width=30,
+buttonDiv = tk.Button(commandFrame, text="/", width=1,
                           command=partial(promptAppend, '/'))
+
+
+###
+### Packing
+###
 
 promptBox.pack()
 buttonAnswer.pack()
 buttonBackspace.pack()
 
+buttonDiv.pack()
+buttonMul.pack()
 buttonPlus.pack()
 buttonMin.pack()
-buttonMul.pack()
-buttonDiv.pack()
 
 buttonOne.pack(side=LEFT)
 buttonTwo.pack(side=LEFT)
@@ -128,6 +156,6 @@ buttonSeven.pack(side=LEFT)
 buttonEight.pack(side=LEFT)
 buttonNine.pack(side=LEFT)
 
-buttonZero.pack()
+buttonZero.pack(side=BOTTOM)
 
 app.mainloop()
